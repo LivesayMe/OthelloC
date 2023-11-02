d008c86033f57be55d90ad009f9d7e5c7b04bc28
Hunter Livesay <livesayme@gmail.com>
2023-11-01 22:04:11 -0400

    Changes package version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup, Extension
 
 setup(
     name='othelloc',
-    version='0.1.0',
+    version='0.2.0',
     packages=['othello'],
     package_data={
         'othello': ['*.so'],
